968120337d029acbcea9cb282a93061a46f737a8
greeve <greg_reeve@byu.edu>
2019-02-27 14:17:16 -0700

    Remove old html and js structure. Change the pack html and js files to the default (index.html and app.js)

--- site/js/app.js
+++ site/js/app.js
@@ -1,115 +1,178 @@
-function bubbleChart() {
-    var width = constants.CHART_WIDTH;
-    var height = constants.CHART_HEIGHT;
+function createBubbleChart(onBubbleClick, data) {
+    console.log(data);
+    var bleed = 50,
+        width = 1024,
+        height = 768;
 
-    var center = { x: width / 2, y: height / 2 };
+    var pack = d3.layout.pack()
+        .sort(null)
+        .size([width, height + bleed * 2])
+        .padding(2);
 
-    var forceStrength = 0.03;
+    var svg = d3.select("#viz").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(0," + -bleed + ")");
 
-    var svg = null;
-    var node = null;
-    var nodes = [];
+    var node = svg.selectAll(".node")
+        .data(pack.nodes(data).filter(function(d) { return !d.children; }))
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .on("click", onBubbleClick);
 
-    function charge(d) {
-        return -Math.pow(d.radius, constants.CHARGE) * forceStrength;
-    }
+    node.append("circle")
+      .attr("r", function(d) { return d.r; });
 
-    var simulation = d3.forceSimulation()
-        .velocityDecay(0.2)
-        .force('x', d3.forceX().strength(forceStrength).x(center.x))
-        .force('y', d3.forceY().strength(forceStrength).y(center.y))
-        .force('charge', d3.forceManyBody().strength(charge))
-        .on('tick', ticked);
-
-    simulation.stop();
-
-    var fillColor = d3.scaleOrdinal(d3.schemePaired);
-
-    function createNodes(data) {
-        var maxAmount = d3.max(data, function (d) { return +d.count; });
-
-        var radiusScale = d3.scalePow()
-            .exponent(0.5)
-            .range([constants.RADIUS_SCALE_MIN, constants.RADIUS_SCALE_MAX])
-            .domain([0, maxAmount]);
-
-        var myNodes = data.map(function (d, index) {
-            return {
-                id: index + 1,
-                name: d.name,
-                slug: d.slug,
-                radius: radiusScale(+d.count),
-                value: +d.count,
-                x: Math.random() * constants.CHART_WIDTH,
-                y: Math.random() * constants.CHART_HEIGHT
-            };
-        });
-
-        myNodes.sort(function (a, b) { return b.count - a.count; });
-
-        return myNodes;
-    }
-
-    var chart = function chart(selector, data) {
-        nodes = createNodes(data);
-
-        svg = d3.select(selector)
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height);
-
-        node = svg.selectAll('circle')
-            .data(nodes, function (d) { return d.id; })
-            .enter().append('g')
-            .on('click', bubbleClick);
-
-        var circle = node.append('circle')
-            .attr('class', 'bubble')
-            .attr('r', 0)
-            .attr('fill', function (d) { return fillColor(d.name); });
-
-        var circleText = node.append('text')
-            .attr('dy', '0.35em')
-            .style('font-size', '1.75em')
-            .attr('fill-opacity', '0')
-            .attr('fill', function (d) { return d3.rgb(fillColor(d.name)).darker(3); })
-            .attr('text-anchor', 'middle')
-            .text(function (d) { return d.name; });
-
-        circleText.transition()
-            .style('fill-opacity', '1')
-            .duration(2000);
-
-        circle.transition()
-            .duration(2000)
-            .attr('r', function (d) { return d.radius; });
-
-        simulation.nodes(nodes);
-
-        simulation.force('x', d3.forceX().strength(forceStrength).x(center.x));
-
-        simulation.alpha(1).restart();
-    }
-
-    function ticked() {
-        node.attr('transform', function (d) {
-            var k = 'translate(' + d.x + ',' + d.y + ')';
-            return k;
-        })
-    }
-
-    return chart;
+    node.append("text")
+      .text(function(d) { return d.name; })
+      .style("font-size", function(d) { return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 24) + "px"; })
+      .attr("dy", ".35em");
 }
 
 function bubbleClick(d) {
-    console.log(d);
-    window.location.href = 'genre.html?' + 'genre=' + d.slug;
+    switch (d.level) {
+        case "genre":
+            // Create a link going to the style level
+            window.location.href = constants.HTML_ROOT + "?" + "genre=" + d.slug;
+            break;
+        case "style":
+            // Create a link going to the decade level
+            window.location.href = constants.HTML_ROOT + "?" + "genre=" + genreParam + "&" + "style=" + d.slug;
+            break;
+        case "decade":
+            // Create a link going to the album level
+            window.location.href = constants.HTML_ROOT + "?" + "genre=" + genreParam + "&" + "style=" + styleParam + "&" + "decade=" + d.name;
+            break;
+        default:
+            console.log("uncaught case")
+    }
+}
+
+function reformatForPack(data, genre, style, decade) {
+    packData = []
+    if (genre !== undefined) {
+        if (style !== undefined) {
+            decadeData = data[genre]["styles"][style]["decades"]
+            for (var key in decadeData) {
+                if (decadeData.hasOwnProperty(key)) {
+                    value = decadeData[key]
+                    packData.push({name: value.name, value: value.count, slug: key, level: "decade"});
+                }
+            }
+        } else {
+            styleData = data[genre]["styles"]
+            for (var key in styleData) {
+                if (styleData.hasOwnProperty(key)) {
+                    value = styleData[key]
+                    packData.push({name: value.name, value: value.count, slug: key, level: "style"});
+                }
+            }
+        }
+    } else {
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                value = data[key]
+                packData.push({name: value.name, value: value.count, slug: key, level: "genre"});
+            }
+        }
+    }
+
+    return {children: packData};
+}
+
+function reformatForTable(data, genre, style, decade) {
+    return data[genre]["styles"][style]["decades"][decade]["albums"];
+}
+
+function createTableChart(data) {
+    console.log(data);
+
+    var table = d3.select("#viz").append("div").attr("class", "table");
+
+    for (var i=0; i < data.length; i++) {
+        var tbodyRow = table.append("a").attr("class", "table-row fa").attr("data-audio", "audio/nbc_chime_diy.mp3");
+        tbodyRow.append("div").attr("class", "table-cell").text(data[i].title);
+        tbodyRow.append("div").attr("class", "table-cell").text(data[i].released);
+        tbodyRow.append("div").attr("class", "table-cell").text(data[i].artists);
+    }
+
+    ;/*SIMPLE AUDIO PLAYER*/(function() {
+        // https://stackoverflow.com/a/34487069/383904
+        var AUD = document.createElement("audio"),
+            BTN = document.querySelectorAll("[data-audio]"),
+            tot = BTN.length;
+
+        function playPause() {
+            // Get track URL from clicked element"s data attribute
+            var src = this.dataset.audio;
+            // Are we already listening that track?
+            if(AUD.src != src) AUD.src = src;
+            // Toggle audio play() / pause() methods
+            AUD[AUD.paused ? "play" : "pause"]();
+            // Remove active class from all other buttons
+            for(var j=0;j<tot;j++) if(BTN[j]!=this) BTN[j].classList.remove("on");
+            // Add active class to clicked button
+            this.classList.toggle("on");
+            // Track ended? (Remove active class etc)
+            AUD.addEventListener("ended", playPause);
+        }
+
+        for(var i=0;i<tot;i++) BTN[i].addEventListener("click", playPause);
+    }());
+
+}
+
+function getURLParameter(name) {
+    var url = new URL(location);
+    return url.searchParams.get(name);
 }
 
 // Start Here
 
-var genreBubbleChart = bubbleChart();
+var genreParam = getURLParameter("genre");
+var styleParam = getURLParameter("style");
+var decadeParam = getURLParameter("decade");
 
-var data = [{"slug": "classical", "name": "Classical", "count": 20052}, {"slug": "pop", "name": "Pop", "count": 9258}, {"slug": "folk-world-country", "name": "Folk, World, & Country", "count": 6641}, {"slug": "jazz", "name": "Jazz", "count": 12088}, {"slug": "electronic", "name": "Electronic", "count": 2366}, {"slug": "stage-screen", "name": "Stage & Screen", "count": 4741}, {"slug": "latin", "name": "Latin", "count": 1014}, {"slug": "rock", "name": "Rock", "count": 7235}, {"slug": "blues", "name": "Blues", "count": 1073}, {"slug": "reggae", "name": "Reggae", "count": 122}, {"slug": "funk-soul", "name": "Funk / Soul", "count": 2363}, {"slug": "non-music", "name": "Non-Music", "count": 980}, {"slug": "children-s", "name": "Children's", "count": 469}, {"slug": "brass-military", "name": "Brass & Military", "count": 390}, {"slug": "hip-hop", "name": "Hip Hop", "count": 424}]
+var level;
+var data;
 
-genreBubbleChart('#viz', data);
+var genreURL = constants.HTML_ROOT;
+var styleURL =  genreURL + "?genre=" + genreParam;
+var decadeURL = styleURL + "&style=" + styleParam;
+
+var nav = d3.select("nav");
+nav.append("a").attr("class", "nav-genre").attr("href", genreURL).append("span").html("Genres");
+
+if (genreParam) {
+    nav.append("a").attr("class", "nav-style").attr("href", styleURL).append("span").html(musicviz[genreParam].name);
+}
+
+if (genreParam && styleParam) {
+    nav.append("a").attr("class", "nav-decade").attr("href", decadeURL).append("span").html(musicviz[genreParam].styles[styleParam].name);
+}
+
+if (decadeParam) {
+    nav.append("a").attr("class", "nav-album").attr("href", window.location.href).append("span").html(decadeParam);
+}
+
+if (decadeParam === null && styleParam === null && genreParam === null) {
+    level = "genre";
+    data = reformatForPack(musicviz);
+} else if (decadeParam === null && styleParam === null && genreParam !== null) {
+    level = "style";
+    data = reformatForPack(musicviz, genreParam);
+} else if (decadeParam === null && styleParam !== null && genreParam !== null) {
+    level = "decade";
+    data = reformatForPack(musicviz, genreParam, styleParam);
+} else if (decadeParam !== null && styleParam !== null && genreParam !== null) {
+    level = "album";
+    data = reformatForTable(musicviz, genreParam, styleParam, decadeParam);
+}
+
+if (level === "genre" || level === "style" || level === "decade") {
+    createBubbleChart(bubbleClick, data);
+} else if (level === "album") {
+   createTableChart(data);
+}
--- site/js/constants.js
+++ site/js/constants.js
@@ -3,5 +3,6 @@ var constants = {
     CHART_HEIGHT: 732,
     CHARGE: 2.1,
     RADIUS_SCALE_MIN: 25,
-    RADIUS_SCALE_MAX: 100
+    RADIUS_SCALE_MAX: 100,
+    HTML_ROOT: "index.html"
 }
