e1f9efa1feb0e10a36f7ffeffc21d1fca136934b
greeve <greg_reeve@byu.edu>
2019-05-29 10:59:04 -0600

    Update the app.js and style.css to add an audio player page that uses HTML5 audio

--- js/app.js
+++ js/app.js
@@ -87,6 +87,15 @@ function reformatForTable(data, genre, style, decade) {
     return data[genre]["styles"][style]["decades"][decade]["albums"];
 }
 
+function reformatForSong(data, genre, style, decade, song) {
+    albums = data[genre]["styles"][style]["decades"][decade]["albums"];
+    for (var i in albums) {
+        if (albums[i].catalog_id === song) {
+            return albums[i];
+        }
+    }
+}
+
 function createTableChart(data) {
     console.log(data);
 
@@ -97,7 +106,8 @@ function createTableChart(data) {
     for (var i=0; i < data.length; i++) {
         if (data[i].audio_filepath) {
             var tbodyRow = table.append("a");
-            tbodyRow.attr("class", "table-row fa").attr("data-audio", "audio/" + data[i].audio_filepath);
+            var songURL = constants.HTML_ROOT + "?" + "genre=" + genreParam + "&" + "style=" + styleParam + "&" + "decade=" + decadeParam + "&" + "song=" + data[i].catalog_id;
+            tbodyRow.attr("class", "table-row fa").attr("data-audio", "audio/" + data[i].audio_filepath).attr("href", songURL);
         } else {
             // var tbodyRow = table.append("span");
             // tbodyRow.attr("class", "table-row fa no-audio");
@@ -109,31 +119,22 @@ function createTableChart(data) {
         tbodyRow.append("div").attr("class", "table-cell").text(data[i].released);
         tbodyRow.append("div").attr("class", "table-cell").text(data[i].artists);
     }
+}
 
-    ;/*SIMPLE AUDIO PLAYER*/(function() {
-        // https://stackoverflow.com/a/34487069/383904
-        var AUD = document.createElement("audio"),
-            BTN = document.querySelectorAll("[data-audio]"),
-            tot = BTN.length;
+function createMusicPlayer(data) {
+    console.log(data);
 
-        function playPause() {
-            // Get track URL from clicked element"s data attribute
-            var src = this.dataset.audio;
-            // Are we already listening that track?
-            if(AUD.src != src) AUD.src = src;
-            // Toggle audio play() / pause() methods
-            AUD[AUD.paused ? "play" : "pause"]();
-            // Remove active class from all other buttons
-            for(var j=0;j<tot;j++) if(BTN[j]!=this) BTN[j].classList.remove("on");
-            // Add active class to clicked button
-            this.classList.toggle("on");
-            // Track ended? (Remove active class etc)
-            AUD.addEventListener("ended", playPause);
-        }
+    var playerContainer = d3.select("#viz").append("div").attr("class", "player");
 
-        for(var i=0;i<tot;i++) BTN[i].addEventListener("click", playPause);
-    }());
+    var title = playerContainer.append("h2").html(data.title);
+    var artists = playerContainer.append("h1").html(data.artists);
+    var released = playerContainer.append("h3").html(data.released);
 
+    var audio = playerContainer.append("audio");
+    audio.attr("controls", "");
+    audio.attr("autoplay", "");
+    var source = audio.append("source");
+    source.attr("src", "audio/" + data.audio_filepath).attr("type", "audio/mpeg");
 }
 
 function getURLParameter(name) {
@@ -146,6 +147,7 @@ function getURLParameter(name) {
 var genreParam = getURLParameter("genre");
 var styleParam = getURLParameter("style");
 var decadeParam = getURLParameter("decade");
+var songParam = getURLParameter("song");
 
 var level;
 var data;
@@ -153,6 +155,7 @@ var data;
 var genreURL = constants.HTML_ROOT;
 var styleURL =  genreURL + "?genre=" + genreParam;
 var decadeURL = styleURL + "&style=" + styleParam;
+var songURL = decadeURL + "&song=" + songParam;
 
 var nav = d3.select("nav");
 nav.append("a").attr("href", genreURL).append("span").attr("class", "genre").html("Genres");
@@ -166,25 +169,45 @@ if (genreParam && styleParam) {
 }
 
 if (decadeParam) {
-    nav.append("a").attr("href", window.location.href).append("span").attr("class", "album").html(decadeParam);
+    var decadeURL;
+
+    if (songParam) {
+        var url = new URL(window.location);
+        var params = new URLSearchParams(url.search);
+        params.delete("song");
+        decadeURL = url.origin + url.pathname + "?" + params.toString();
+    } else {
+        decadeURL = window.location.href;
+
+    }
+    nav.append("a").attr("href", decadeURL).append("span").attr("class", "album").html(decadeParam);
 }
 
-if (decadeParam === null && styleParam === null && genreParam === null) {
+if (songParam) {
+    //nav.append("a").attr("href", window.location.href).append("span").attr("class", "song").html(songParam);
+}
+
+if (songParam === null && decadeParam === null && styleParam === null && genreParam === null) {
     level = "genre";
     data = reformatForPack(musicviz);
-} else if (decadeParam === null && styleParam === null && genreParam !== null) {
+} else if (songParam === null && decadeParam === null && styleParam === null && genreParam !== null) {
     level = "style";
     data = reformatForPack(musicviz, genreParam);
-} else if (decadeParam === null && styleParam !== null && genreParam !== null) {
+} else if (songParam === null && decadeParam === null && styleParam !== null && genreParam !== null) {
     level = "decade";
     data = reformatForPack(musicviz, genreParam, styleParam);
-} else if (decadeParam !== null && styleParam !== null && genreParam !== null) {
+} else if (songParam === null && decadeParam !== null && styleParam !== null && genreParam !== null) {
     level = "album";
     data = reformatForTable(musicviz, genreParam, styleParam, decadeParam);
+} else if (songParam !== null && decadeParam !== null && styleParam !== null && genreParam !== null) {
+    level = "song";
+    data = reformatForSong(musicviz, genreParam, styleParam, decadeParam, songParam);
 }
 
 if (level === "genre" || level === "style" || level === "decade") {
     createBubbleChart(bubbleClick, data, level);
 } else if (level === "album") {
-   createTableChart(data);
+    createTableChart(data);
+} else if (level === "song") {
+    createMusicPlayer(data);
 }
